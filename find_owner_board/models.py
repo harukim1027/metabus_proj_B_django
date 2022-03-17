794ef140e81d9ad9024e7ea63287cf7fad4ee3fa
Suin Kim <soo.officer@gmail.com>
2022-03-17 11:29:11 +0900

    find_owner_board 모델변경

--- find_owner_board/models.py
+++ find_owner_board/models.py
@@ -2,6 +2,14 @@ from django.conf import settings
 from django.db import models
 
 from accounts.models import User
+from django.core.exceptions import ValidationError
+
+
+def validate_image(image):
+    file_size = image.size
+    limit_mb = 3
+    if file_size > limit_mb * 1024 * 1024:
+        raise ValidationError("이미지의 최대 크기는 %s MB 입니다." % limit_mb)
 
 
 class TimestampedModel(models.Model):
@@ -12,6 +20,7 @@ class TimestampedModel(models.Model):
         abstract = True
 
 
+# 주인찾습니다 게시판
 class FindOwnerBoard(TimestampedModel):
     find_board_no = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50, db_index=True)
@@ -24,19 +33,30 @@ class FindOwnerBoard(TimestampedModel):
     find_time = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['-find_board_no']
 
+
+# 주인찾습니다 게시판 댓글
 class FindOwnerBoardComment(TimestampedModel):
     find_comment_no = models.AutoField(primary_key=True)
     comment_content = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     find_board_no = models.ForeignKey(FindOwnerBoard, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.comment_content
 
+    class Meta:
+        ordering = ['find_comment_no']
+
+
+# 주인찾습니다 이미지
 class FindOwnerBoardImage(TimestampedModel):
     find_image_no = models.AutoField(primary_key=True)
-    image1 = models.TextField()
-    image2 = models.TextField(blank=True)
-    image3 = models.TextField(blank=True)
-    find_board_no = models.ForeignKey(FindOwnerBoard, on_delete=models.CASCADE)
+    image = models.ImageField(blank=False, null=False, validators=[validate_image])
+    find_board_no = models.ForeignKey(FindOwnerBoard, on_delete=models.CASCADE, related_name="board_image")
 
+    class Meta:
+        ordering = ['-find_image_no']
 
--- find_owner_board/views.py
+++ find_owner_board/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from find_owner_board.models import FindOwnerBoard
 from find_owner_board.serializers import FindOwnerBoardSerializer, FindOwnerBoardCreateSerializer
-from find_owner_board.serializers import FindOwnerBoardCommentSerializer, FindOwnerBoardCommentCreateSerializer
-from find_owner_board.serializers import FindOwnerBoardImageSerializer, FindOwnerBoardImageCreateSerializer
-from find_owner_board.models import FindOwnerBoard, FindOwnerBoardComment, FindOwnerBoardImage
 
 
 class FindOwnerBoardViewSet(viewsets.ModelViewSet):
@@ -15,23 +15,8 @@ class FindOwnerBoardViewSet(viewsets.ModelViewSet):
             return FindOwnerBoardSerializer
         return FindOwnerBoardCreateSerializer
 
-
-class FindOwnerBoardCommentViewSet(viewsets.ModelViewSet):
-    queryset = FindOwnerBoardComment.objects.all()
-
-    def get_serializer_class(self):
+    def get_permissions(self):
         method = self.request.method
         if method == "GET":
-            return FindOwnerBoardCommentSerializer
-        return FindOwnerBoardCommentCreateSerializer
-
-
-class FindOwnerBoardImageViewSet(viewsets.ModelViewSet):
-    queryset = FindOwnerBoardImage.objects.all()
-
-    def get_serializer_class(self):
-        method = self.request.method
-        if method == "GET":
-            return FindOwnerBoardImageSerializer
-        return FindOwnerBoardImageCreateSerializer
-
+            return [AllowAny()]
+        return [IsAuthenticated()]
