1b7dc86146b0a9886e570bdb5647a182c03ef92f
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-03-17 11:11:44 +0900

    1) adopt_assignment admin 등록 2) notice image 필드, file 필드로 분리 후 serializers 추가

--- notice/serializers.py
+++ notice/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from notice.models import Notice
+from notice.models import Notice, NoticeFile, NoticeImage
 
 
 class NoticeCreateSerializer(serializers.ModelSerializer):
@@ -8,12 +8,28 @@ class NoticeCreateSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class NoticeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoticeImage
+        fields = "__all__"
+
+
+class NoticeFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoticeFile
+        fields = "__all__"
+
+
 class NoticeSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    notice_image = NoticeImageSerializer(read_only=True)
+    notice_file = NoticeFileSerializer(read_only=True)
+
 
     class Meta:
         model = Notice
         fields = "__all__"
         depth = 1
+
 
