59a678a9887439428c4bf8e49f7c4836240d2cca
Suin Kim <soo.officer@gmail.com>
2022-03-17 13:12:16 +0900

    serializers.py 누락된 부분 추가

--- find_owner_board/serializers.py
+++ find_owner_board/serializers.py
@@ -18,7 +18,7 @@ class FindOwnerBoardSerializer(serializers.ModelSerializer):
     lost_time = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-    board_image = FindOwnerBoardImageSerializer(read_only=True)
+    board_image = FindOwnerBoardImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = FindOwnerBoard
