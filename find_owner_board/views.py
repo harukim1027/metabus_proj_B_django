b6ffb31404e1e622f4985c888bdece0ab9db3d09
Suin Kim <soo.officer@gmail.com>
2022-03-21 23:57:48 +0900

    find_owner_board, lost_pet_board/ admin, views.py 수정

--- find_owner_board/views.py
+++ find_owner_board/views.py
@@ -5,16 +5,31 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from find_owner_board.models import FindOwnerBoard, FindOwnerBoardImage
 from find_owner_board.serializers import FindOwnerBoardSerializer, FindOwnerBoardCreateSerializer
 from find_owner_board.serializers import FindOwnerBoardImageSerializer, FindOwnerBoardImageCreateSerializer
+from notice.paginations.Pagination import Pagination
 
 
 class FindOwnerBoardViewSet(viewsets.ModelViewSet):
     queryset = FindOwnerBoard.objects.all()
+    pagination_class = Pagination
 
     def get_serializer_class(self):
         method = self.request.method
         if method == "GET":
             return FindOwnerBoardSerializer
         return FindOwnerBoardCreateSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        location = self.request.query_params.get("location", "")
+        if location:
+            qs = qs.filter(find_location__icontains=location)
+
+        animaltype = self.request.query_params.get("animaltype", "")
+        if animaltype:
+            qs = qs.filter(animal_type__icontains=animaltype)
+
+        return qs
 
     def get_permissions(self):
         method = self.request.method
--- lost_pet_board/views.py
+++ lost_pet_board/views.py
@@ -4,16 +4,31 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from lost_pet_board.models import LostPetBoard, LostPetBoardImage
 from lost_pet_board.serializers import LostPetBoardSerializer, LostPetBoardCreateSerializer
 from lost_pet_board.serializers import LostPetBoardImageSerializer, LostPetBoardImageCreateSerializer
+from notice.paginations.Pagination import Pagination
 
 
 class LostPetViewSet(viewsets.ModelViewSet):
     queryset = LostPetBoard.objects.all()
+    pagination_class = Pagination
 
     def get_serializer_class(self):
         method = self.request.method
         if method == "GET":
             return LostPetBoardSerializer
         return LostPetBoardCreateSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        location = self.request.query_params.get("location", "")
+        if location:
+            qs = qs.filter(lost_location__icontains=location)
+
+        animaltype = self.request.query_params.get("animaltype", "")
+        if animaltype:
+            qs = qs.filter(animal_type__icontains=animaltype)
+
+        return qs
 
     def get_permissions(self):
         method = self.request.method
