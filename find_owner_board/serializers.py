33d641a0881bdf258e52b23df1882c96904b104e
Suin Kim <soo.officer@gmail.com>
2022-03-29 14:33:14 +0900

    마이그레이션 초기화 후 find_owner_board,lost_pet_board/ models,view,serializers.py 변경

--- find_owner_board/serializers.py
+++ find_owner_board/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from find_owner_board.models import FindOwnerBoard, FindOwnerBoardImage
 
 
-class FindOwnerBoardCreateSerializer(serializers.ModelSerializer):
+class FindOwnerBoardImageCreateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = FindOwnerBoard
+        model = FindOwnerBoardImage
         fields = "__all__"
 
 
@@ -15,11 +15,28 @@ class FindOwnerBoardImageSerializer(serializers.ModelSerializer):
 
 
 class FindOwnerBoardSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
-    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
     board_image = FindOwnerBoardImageSerializer(many=True, read_only=True)
 
     class Meta:
         model = FindOwnerBoard
-        fields = ["find_board_no", "title", "content", "animal_type", "breed", "size",
+        fields = "__all__"
+
+
+class FindOwnerBoardCreateSerializer(serializers.ModelSerializer):
+    # created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    # updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    board_image = FindOwnerBoardImageSerializer(many=True, read_only=True)
+    # 기본적으로 중첩된 Serializer 에서는 쓰기(Create), 갱신(Update)를 지원해주지 않음. 그래서 read_only=True 옵션을 넣음
+
+    class Meta:
+        model = FindOwnerBoard
+        fields = ["find_board_no", "title", "status", "content", "animal_type", "dog_breed", "cat_breed", "size", "sex",
                   "animal_tag", "find_location", "find_time", "board_image", "user", "created_at", "updated_at"]
+
+        def create(self, validated_data):
+            images = self.context['request'].FILES.getlist('board_image')
+
+            instance = FindOwnerBoard.objects.create(**validated_data)
+            for image in images:
+                FindOwnerBoardImage.objects.create(find_board_no=instance, image=image)
+                return instance
--- find_owner_board/views.py
+++ find_owner_board/views.py
@@ -1,19 +1,79 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-from find_owner_board.models import FindOwnerBoard
+from find_owner_board.models import FindOwnerBoard, FindOwnerBoardImage
 from find_owner_board.serializers import FindOwnerBoardSerializer, FindOwnerBoardCreateSerializer
+from find_owner_board.serializers import FindOwnerBoardImageSerializer, FindOwnerBoardImageCreateSerializer
+from notice.paginations.Pagination import Pagination
 
 
 class FindOwnerBoardViewSet(viewsets.ModelViewSet):
     queryset = FindOwnerBoard.objects.all()
+    pagination_class = Pagination
 
     def get_serializer_class(self):
         method = self.request.method
         if method == "GET":
             return FindOwnerBoardSerializer
         return FindOwnerBoardCreateSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        location = self.request.query_params.get("location", "")
+        if location:
+            qs = qs.filter(find_location__icontains=location)
+
+        animaltype = self.request.query_params.get("animaltype", "")
+        if animaltype:
+            qs = qs.filter(animal_type__icontains=animaltype)
+
+        status = self.request.query_params.get("status", "")
+        if status:
+            qs = qs.filter(status__icontains=status)
+
+        author = self.request.query_params.get("author", "")
+        if author:
+            qs = qs.filter(user__userID__icontains=author)
+
+        return qs
+
+    def get_permissions(self):
+        method = self.request.method
+        if method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def create(self, request, *args, **kwargs):
+
+        image_data = request.data["board_image"]
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors)
+
+        find_board_no = serializer.data["find_board_no"]
+
+        image_serializer = FindOwnerBoardImageSerializer(data={"image": image_data,
+                                                               "find_board_no": find_board_no})
+
+        if image_serializer.is_valid():
+            image_serializer.save()
+
+        return Response(serializer.data)
+
+
+class FindOwnerBoardImageViewSet(viewsets.ModelViewSet):
+    queryset = FindOwnerBoardImage.objects.all()
+
+    def get_serializer_class(self):
+        method = self.request.method
+        if method == "GET":
+            return FindOwnerBoardImageSerializer
+        return FindOwnerBoardImageCreateSerializer
 
     def get_permissions(self):
         method = self.request.method
