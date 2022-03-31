97ddba5823da8479a6215236314082f1f672a7bf
namuns <skymsn214@gmail.com>
2022-03-31 15:30:03 +0900

    review rank api를 위한 view와 urls 수정

--- adopt_review/urls.py
+++ adopt_review/urls.py
@@ -2,13 +2,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from adopt_review import views
-from adopt_review.views import ReviewViewSet, CommentViewset
+from adopt_review.views import ReviewViewSet, CommentViewset, ReviewAllViewSet
 
 app_name = "adopt_review"
 
 router = DefaultRouter()
 router.register("reviews", ReviewViewSet)
 router.register("comments", CommentViewset)
+router.register("allreviews", ReviewAllViewSet)
+
 
 
 urlpatterns = [
--- adopt_review/views.py
+++ adopt_review/views.py
@@ -39,3 +39,28 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewset(viewsets.ModelViewSet):
     queryset = AdoptReviewComment.objects.all()
     serializer_class = CommentSerializer
+
+
+class ReviewAllViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+
+    def get_serializer_class(self):
+        method = self.request.method
+        if method == "GET":
+            return ReviewSerializer
+        return ReviewCreateSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        query = self.request.query_params.get("query", "")
+        if query:
+            qs = qs.filter(animal_reg_num__icontains=query)
+
+        return qs
+
+    def get_permissions(self):
+        method = self.request.method
+        if method == "GET" or method == "PATCH":
+            return [AllowAny()]
+        return [IsAuthenticated()]
