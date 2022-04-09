3e1427ade00afc6e5005d8e0a91b6993689877bf
Suin Kim <soo.officer@gmail.com>
2022-04-09 10:22:17 +0900

    notice 이미지,첨부파일 따로 저장 구현중

--- notice/serializers.py
+++ notice/serializers.py
@@ -2,19 +2,25 @@ from rest_framework import serializers
 from notice.models import Notice, NoticeFile, NoticeImage
 
 
-class NoticeCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Notice
-        fields = "__all__"
-
-
 class NoticeImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoticeImage
         fields = "__all__"
 
 
+class NoticeImageCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoticeImage
+        fields = "__all__"
+
+
 class NoticeFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoticeFile
+        fields = "__all__"
+
+
+class NoticeFileCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = NoticeFile
         fields = "__all__"
@@ -32,3 +38,24 @@ class NoticeSerializer(serializers.ModelSerializer):
         depth = 1
 
 
+class NoticeCreateSerializer(serializers.ModelSerializer):
+    notice_image = NoticeImageSerializer(many=True, read_only=True)
+    notice_file = NoticeFileSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Notice
+        fields = "__all__"
+
+    def create(self, validated_data):
+        images = self.context['request'].FILES.getlist('notice_image')
+        files = self.context['request'].FILES.getlist('notice_file')
+
+        instance = Notice.objects.create(**validated_data)
+        for image in images:
+            NoticeImage.objects.create(notice_no=instance, image=image)
+        # return instance
+        #
+        # instance = Notice.objects.create(**validated_data)
+        for file in files:
+            NoticeFile.objects.create(notice_no=instance, file=file)
+        return instance
--- notice/urls.py
+++ notice/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from notice.views import NoticeViewSet
-
+from notice.views import NoticeViewSet, NoticeImageViewSet, NoticeFileViewSet
 
 app_name = 'notice'
 
 router = DefaultRouter()
 router.register("notices", NoticeViewSet)
+router.register("images", NoticeImageViewSet)
+router.register("files", NoticeFileViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
